589397dbf2e4d475ace109d13805a4c808c18414
Jake Jung <jakej@gwazoo.com>
2018-09-27 21:17:42 -0600

    init

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import TestScreen from '../screens/TestScreen';
+
+const TestScreenStack = createStackNavigator({
+  Home: TestScreen,
+});
+
+TestScreenStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
+
+export default createBottomTabNavigator({
+  TestScreenStack
+});
